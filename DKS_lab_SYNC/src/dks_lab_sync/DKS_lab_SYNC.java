d3e61ef077b2be7b4ded7346d984c234f6d8588a
HoYoung <ghdud2419@naver.com>
2016-08-04 22:18:09 +0900

    CHANGES !

--- DKS_lab_SYNC/src/dks_lab_sync/DKS_lab_SYNC.java
+++ DKS_lab_SYNC/src/dks_lab_sync/DKS_lab_SYNC.java
@@ -15,6 +15,7 @@ public class DKS_lab_SYNC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        "SEOHOYUNG"
         // TODO code application logic here
     }
     
